2127436af56300f9eae9ed47b44bcff8c6087799
Xue Wei Huang <chauvet1302@gmail.com>
2019-07-18 22:05:50 -0400

    complete basic setup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/models/recipe';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[]=[
+    new Recipe('Test recipe', 'this is a test', 'https://matchasource.com/wp-content/uploads/2015/10/matchamor30_3MOR30-1.jpg') ,
+    new Recipe('Test recipe', 'this is a test', 'https://matchasource.com/wp-content/uploads/2015/10/matchamor30_3MOR30-1.jpg') 
+
+  ];
 
   constructor() { }
 
